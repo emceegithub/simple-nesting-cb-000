3ba344190229a05bd8d305830948bbab91b02612
Mark Cruz <markchriscruz@yahoo.com>
2018-03-08 01:59:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,8 @@ def adding_matz
 
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = 
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  return programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
